d63f7f363f7bf63669f44f685495e58e660d5fae
Muhammed Uluyol <uluyol0@gmail.com>
2015-11-01 00:57:20 -0400

    add docs

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package rgo provides a mechanism to call into R. This package assumes that
+you have the R binary in your PATH and that you have the jsonlite and RCurl
+R packages installed.
+*/
+package rgo
