09bbc64f1a9fa736f1b050c46a4027e307ce1c23
Muhammed Uluyol <uluyol0@gmail.com>
2016-01-07 15:27:48 -0500

    Generalize data frame a bit (ugly)

--- dataframe.go
+++ dataframe.go
@@ -1,24 +1,77 @@
 package rgo
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+)
 
-// DataFrames are used to hold tabular float64 data. This
-// type can be easily marshaled into an R dataframe. This
-// type is still experimental.
+// SimpleData is the the data type used for DataFrames.
+// A string, int, float, or bool (of any size) is a
+// valid value for a SimpleData variable.
+type SimpleData interface{}
+
+func ensureSimpleData(x SimpleData) {
+	switch x.(type) {
+	case string, bool:
+	case int, int8, int16, int32, int64: // include rune and byte
+	case uint, uint8, uint16, uint32, uint64, uintptr:
+	case float32, float64:
+	default:
+		panic(fmt.Sprintf("%s is not a valid SimpleData value", x))
+	}
+}
+
+// DataFrames are used to hold tabular data. This type
+// can be easily marshaled into an R dataframe.
+// DataFrames can only store data that meet the
+// requirements set by SimpleData.
+//
+// This type is still experimental.
 type DataFrame struct {
-	cols         [][]float64
+	cols         [][]SimpleData
 	colNames     []string
 	rowNames     []string
 	namelessRows bool
 }
 
 // Remember to update this, MarshalJSON, UnmarshalJSON,
-// and SendDF, when updating DataFrame.
+// SendDF, and validate when updating DataFrame.
 type dataFrameJSON struct {
-	Cols         [][]float64 `json:"cols"`
-	ColNames     []string    `json:"colNames"`
-	RowNames     []string    `json:"rowNames"`
-	NamelessRows bool        `json:"namelessRows"`
+	Cols         [][]SimpleData `json:"cols"`
+	ColNames     []string       `json:"colNames"`
+	RowNames     []string       `json:"rowNames"`
+	NamelessRows bool           `json:"namelessRows"`
+}
+
+func (df *DataFrame) ValidateColumns() (err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			if er, ok := e.(error); ok {
+				err = er
+			} else if estr, ok := e.(string); ok {
+				err = errors.New(estr)
+			} else {
+				panic(err)
+			}
+		}
+	}()
+	for coli, col := range df.cols {
+		if len(col) == 0 {
+			continue
+		}
+
+		ensureSimpleData(col[0])
+		cType := reflect.TypeOf(col[0])
+		for i := 1; i < len(col); i++ {
+			ensureSimpleData(col[i])
+			if !reflect.TypeOf(col[i]).AssignableTo(cType) {
+				return fmt.Errorf("found different data types in column %d", coli)
+			}
+		}
+	}
+	return nil
 }
 
 func (df *DataFrame) MarshalJSON() ([]byte, error) {
@@ -51,10 +104,10 @@ func (df *DataFrame) SetCols(colNames ...string) {
 		panic("already set columns on this dataframe")
 	}
 	df.colNames = colNames
-	df.cols = make([][]float64, len(colNames))
+	df.cols = make([][]SimpleData, len(colNames))
 }
 
-func (df *DataFrame) AppendRow(name string, vals ...float64) {
+func (df *DataFrame) AppendRow(name string, vals ...SimpleData) {
 	if df.namelessRows {
 		panic("cannot add named row: rows are nameless")
 	}
@@ -67,7 +120,7 @@ func (df *DataFrame) AppendRow(name string, vals ...float64) {
 	}
 }
 
-func (df *DataFrame) AppendURow(vals ...float64) {
+func (df *DataFrame) AppendURow(vals ...SimpleData) {
 	if df.rowNames != nil {
 		panic("cannot add nameless row: rows are named")
 	}
